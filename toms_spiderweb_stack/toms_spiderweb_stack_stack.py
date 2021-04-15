18f947371607100dc3d935501647e27b01ed6dbc
Tom Cavanaugh <cavanaugh.tom@principal.com>
2021-04-15 16:24:13 +0000

    Initial solution

---
+++ toms_spiderweb_stack/toms_spiderweb_stack_stack.py
@@ -0,0 +1,23 @@
+from aws_cdk import (
+    core as cdk,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
+)
+
+class TomsSpiderwebStackStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
+        )
+        
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=my_lambda,
+        )
+        
